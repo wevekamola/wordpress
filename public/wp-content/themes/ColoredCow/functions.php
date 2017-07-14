a0aa43190ee0637d0e9f3b8c2f8f746d1cdf52ba
wevekamola <vivek.amola@coloredcow.in>
2017-07-14 14:50:36 +0530

    wordpress_files

---
+++ public/wp-content/themes/ColoredCow/functions.php
@@ -0,0 +1,46 @@
+<?php 
+
+if ( ! function_exists( 'cc_scripts' ) ) {
+    function cc_scripts() {
+        wp_enqueue_script('cc-bootstrap', get_template_directory_uri().'/dist/lib/js/bootstrap.min.js', array('jquery'), '1.0.0', true);
+        wp_enqueue_script('main', get_template_directory_uri().'/main.js', array('jquery', 'cc-bootstrap'), '1.0.0', true);
+        wp_localize_script( 'main', 'PARAMS', array('ajaxurl' => admin_url('admin-ajax.php')) );
+
+    }
+    add_action('wp_enqueue_scripts','cc_scripts');
+}
+
+if ( ! function_exists( 'cc_styles' ) ) {
+    function cc_styles() {  
+        wp_enqueue_style('cc-bootstrap', get_template_directory_uri().'/dist/lib/css/bootstrap.min.css');
+        wp_enqueue_style('style', get_template_directory_uri().'/style.css');
+
+    }
+    add_action('wp_enqueue_scripts','cc_styles');
+}
+
+//add filter to remove margin above html
+add_filter('show_admin_bar','__return_false');
+function wpb_custom_new_menu() {
+
+  register_nav_menus(
+
+    array(
+
+      'my-custom-menu' => __( 'My Custom Menu' ),
+
+      'extra-menu' => __( 'Extra Menu' )
+
+    )
+
+  );
+
+}
+
+add_action( 'init', 'wpb_custom_new_menu' );
+
+
+
+
+
+?>
---
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Template Name: Landing
+ */
+    get_header();
+?>
+
+
+
+
+<?php 
+    get_footer(); 
+?>
